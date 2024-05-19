0a514cdf3b958d5041732eaffc26ef1df9d44afe
Josie <josipakukor@gmail.com>
2024-05-19 19:04:33 +0200

    Welcome page
    Mixcloud players are a draft to be loaded with button pres once designed

---
+++ src/app/title.tsx
@@ -0,0 +1,9 @@
+import styles from "./title.module.css";
+
+export default function MainTitle() {
+  return (
+    <div className={styles.title}>
+      <h1 className={styles.h1}>KUE JAMIE</h1>
+    </div>
+  );
+}
