8e58e65a1dde36386363b3962d7c00285f46d004
Josie <josipakukor@gmail.com>
2024-05-27 01:32:34 +0200

    Add 3D designs and change Title style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,53 @@
+'use client'
 import styles from "./page.module.css";
 import MainTitle from "./title";
+import Spline from "@splinetool/react-spline";
 
 export default function Home() {
   return (
     <main>
       <div className={styles.description}>
         <MainTitle />
+        <div style={{
+          marginTop: 80,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+
+        }}>
+        <Spline scene="https://prod.spline.design/5JypQuGG0lIkcwIT/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/TPpa5eskoSP1Ven5/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/5JypQuGG0lIkcwIT/scene.splinecode" />
+        </div>
       </div>
-      <div className={styles.buttonWrap}>
-        <button className={styles.button}>Latest Mixes</button>
+       
+      <div className={styles.buttonWrap} >
+        <text style={{
+          color: "rgb(188, 181, 245)",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          fontSize: 24,
+          fontFamily: "Bebas Neue",
+          marginTop: -80,
+          zIndex: 4,
+          marginRight: 120
+        }}>LISTEN</text>
+        <text style={{
+          color: "rgb(188, 181, 245)",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          fontSize: 24,
+          fontFamily: "Bebas Neue",
+          marginTop: -80,
+          zIndex: 4
+        }}>BOOK NOW</text>
       </div>
       <div
         style={{
-          marginTop: 800,
+          marginTop: 180,
+          marginBottom: 80,
           display: "flex",
           flexDirection: "row",
           justifyContent: "center",
